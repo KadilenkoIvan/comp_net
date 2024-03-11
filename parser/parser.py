35c5d02d60277ddc58cb83ac34d34fe95ecd99db
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-03-12 00:53:19 +0700

    update parser

--- parser/parser.py
+++ parser/parser.py
@@ -7,31 +7,47 @@ browser.maximize_window()
 browser.implicitly_wait(5)
 browser.get("https://www.citilink.ru")
 
+#what you whant to search
+product_serach = "Ноутбук"
+
+#click on cookie button if have
 try:
-    browser.find_element(By.XPATH, "/html/body/div[2]/div/div[5]/div[1]/div/button/span").click()
+    browser.find_element(By.XPATH, '//button/span[text()="Я согласен"]').click()
 except:
     print("no coockie button")
 
-xpath = "/html/body/div[2]/div/main/div[1]/div[1]/div/div[2]/div/div[2]/div/a[2]"
-browser.find_element(By.XPATH, xpath).click()
+#write to "search" form and search
+browser.find_element(By.XPATH, "//input[@type='search']").send_keys(product_serach)
+browser.find_element(By.XPATH, "//button[@type='submit']").click()
 
-class_name = ".app-catalog-7fwdng.e8kvwwz0"
-smartphones = browser.find_elements(By.CSS_SELECTOR, class_name)
-#print(smartfones)
-with open("sitilinc_smartphones_info.csv", "w") as file:
-    i = 0
-    for smartphone in smartphones:
-        name = '\"' + smartphone.find_element(By.CSS_SELECTOR, ".app-catalog-1tp0ino.e1k5a7g60").text + '\"'
-        characteristics = smartphone.find_elements(By.CSS_SELECTOR, ".app-catalog-17ju59h.e4qu3682")
-        characteristics_list = []
+#go to detailed mode
+browser.find_element(By.XPATH, "//label[@for='Подробный режим каталога-list']").click()
+
+#get info and write to csv
+class_name = ".e12wdlvo0.app-catalog-1bogmvw.e1loosed0"
+products = browser.find_elements(By.CSS_SELECTOR, class_name)
+
+with open("sitilinc_info.csv", "w", encoding="utf-16") as file:
+    file.write("\"id\",\"name\",\"url\",\"characteristics\",\"price\"\n")
+    i = 1
+    for product in products:
+        #get name and url
+        name_url = product.find_element(By.XPATH, ".//div/div[3]/div[1]/a")
+        name = "\"" + name_url.text.replace("\"", "\'") + "\""
+        url = name_url.get_attribute("href")
+
+        #get characteristics 
+        characteristics = product.find_elements(By.XPATH, ".//div/div[6]/ul/li")
+        full_characteristic = "\""
         for characteristic in characteristics:
-            characteristics_list.append(characteristic.text)
-        try:
-            display = '\"' + characteristics_list[0] + '\"'
-            processor = '\"' + characteristics_list[1] + '\"'
-            memory = '\"' + characteristics_list[2] + '\"'
-            size = '\"' + characteristics_list[5] + '\"'
-            file.write(str(i) + ',' + name + ',' + display + ',' + processor + ',' + memory + ',' + size + '\n')
-            i+=1
-        except:
-            continue
+            full_characteristic += characteristic.text.replace("\"", "\'") + " "
+        full_characteristic += "\""
+
+        #get price
+        price = "\"" + product.find_element(By.XPATH, ".//div/div[7]/div/div[2]").text + "\""
+        
+        #write to file
+        info = str(i) + ',' + name + ',' + url + ',' + full_characteristic + ',' + price + '\n' 
+        print(info)
+        file.write(info)
+        i+=1
